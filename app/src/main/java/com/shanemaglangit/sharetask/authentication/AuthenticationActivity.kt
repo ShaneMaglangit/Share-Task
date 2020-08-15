4c6c43c31c5c066730dd3fd13d3d460943ea59cb
ShaneMaglangit <shanedmaglangit@gmail.com>
2020-08-16 01:10:38 +0800

    Added Initial Layout for Tasks and Task Items

--- app/src/main/java/com/shanemaglangit/sharetask/authentication/AuthenticationActivity.kt
+++ app/src/main/java/com/shanemaglangit/sharetask/authentication/AuthenticationActivity.kt
@@ -2,8 +2,6 @@ package com.shanemaglangit.sharetask.authentication
 
 import android.content.SharedPreferences
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.shanemaglangit.sharetask.R
 import com.shanemaglangit.sharetask.util.startMainActivity
@@ -19,8 +17,8 @@ class AuthenticationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
 
-//        if(sharedPreferences.getBoolean("visited", false)) {
-//            startMainActivity()
-//        }
+        if (sharedPreferences.getBoolean("visited", false)) {
+            startMainActivity()
+        }
     }
 }
--- app/src/main/java/com/shanemaglangit/sharetask/main/MainActivity.kt
+++ app/src/main/java/com/shanemaglangit/sharetask/main/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.shanemaglangit.sharetask.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.shanemaglangit.sharetask.R
+import com.shanemaglangit.sharetask.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.main_nav_host) as NavHostFragment
+        NavigationUI.setupWithNavController(binding.bottomNavMain, navHost.navController)
     }
 }
